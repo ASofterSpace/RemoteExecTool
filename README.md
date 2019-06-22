b2bbd97703051da0cc538dca90c476afa3efe747
Tom Moya Schiller <moya@asofterspace.com>
2019-06-22 22:57:48 +0200

    Add initial skeleton commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# RemoteExecTool
+# Remote Exec Tool
+
+**Class:** A Softer Space Internal
+
+**Language:** Java
+
+**Platform:** Windows / Linux
+
 This project enables remotely operating computery devices in fun ways.
+
+## Setup
+
+Download our Toolbox-Java (which is a separate project here on github) into an adjacent directory on your hard drive.
+
+Start the build by calling under Windows:
+
+```
+build.bat
+```
+
+Or under Linux:
+
+```
+build.sh
+```
+
+## Configure
+
+Copy the built program onto all the computery devices that are supposed to be part of the fun.
+
+Put into the file `config/config.json` something like:
+
+```
+{
+	"name": "Moya-VII /Odyssey/",
+	"id": 1,
+	"sharePath": "Z:\remoteExecTool\",
+	"commands": [
+		{
+			"shorthand": "beat",
+			"kind": "shellexec",
+			"path": "C:\Programs (x86)\Steam\steamapps\common\Beat Saber\Beat Saber.exe"
+		}
+	]
+}
+```
+
+Ensure that the ids of all computery devices are unique!
+
+## Run
+
+To start up the Remote Exec Tool after it has been built, you can call under Windows:
+
+```
+run.bat
+```
+
+Or under Linux:
+
+```
+run.sh
+```
+
+## License
+
+We at A Softer Space really love the Unlicense, which pretty much allows anyone to do anything with this source code.
+For more info, see the file UNLICENSE.
+
+If you desperately need to use this source code under a different license, [contact us](mailto:moya@asofterspace.com) - I am sure we can figure something out.
---
+++ src/com/asofterspace/remoteExecTool/Main.java
@@ -0,0 +1,56 @@
+/**
+ * Unlicensed code created by A Softer Space, 2019
+ * www.asofterspace.com/licenses/unlicense.txt
+ */
+package com.asofterspace.remoteExecTool;
+
+import com.asofterspace.toolbox.configuration.ConfigFile;
+import com.asofterspace.toolbox.io.Directory;
+import com.asofterspace.toolbox.io.File;
+import com.asofterspace.toolbox.io.Record;
+import com.asofterspace.toolbox.Utils;
+
+import java.util.List;
+
+
+public class Main {
+
+	public final static String PROGRAM_TITLE = "RemoteExecTool";
+	public final static String VERSION_NUMBER = "0.0.0.1(" + Utils.TOOLBOX_VERSION_NUMBER + ")";
+	public final static String VERSION_DATE = "22. June 2019 - 22. June 2019";
+
+	public static void main(String[] args) {
+
+		// let the Utils know in what program it is being used
+		Utils.setProgramTitle(PROGRAM_TITLE);
+		Utils.setVersionNumber(VERSION_NUMBER);
+		Utils.setVersionDate(VERSION_DATE);
+
+		if (args.length > 0) {
+			if (args[0].equals("--version")) {
+				System.out.println(Utils.getFullProgramIdentifierWithDate());
+				return;
+			}
+
+			if (args[0].equals("--version_for_zip")) {
+				System.out.println("version " + Utils.getVersionNumber());
+				return;
+			}
+		}
+
+		System.out.println("Loading configuration...");
+
+		ConfigFile config = new ConfigFile("config.json");
+
+		Integer id = config.getInteger("id");
+
+		String name = config.getValue("name");
+
+		String sharePath = config.getValue("sharePath");
+
+		List<Record> commands = config.getAllContents().getArray("commands");
+
+		System.out.println("This is the RemoteExecTool with id " + id + " on computery device " + name + "! :)");
+	}
+
+}
